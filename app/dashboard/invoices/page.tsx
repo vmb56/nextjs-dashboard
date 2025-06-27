168375581c8b7f7e71f8a7526e9c5b3e488b511d
valy bamba <valybamba56@gmail.com>
2025-06-27 18:01:08 +0200

    First

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+  return(<p>invoices pages page</p>);
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import '@/app/ui/global.css'
+import {lusitana} from'@/app/ui/fonts'
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="fr" >
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
